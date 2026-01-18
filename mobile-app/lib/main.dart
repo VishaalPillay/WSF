689e7f357d35c6f1448c36bf26a4b5f448006cb8
Nikhil <nikhilbalamurugan@gmail.com>
2026-01-18 20:44:41 +0530

    added login page

--- mobile-app/lib/main.dart
+++ mobile-app/lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
-import 'screens/home_screen.dart';
+import 'screens/login_screen.dart'; 
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   
   // Pass your Mapbox Token here. 
-  // In a real app, use --dart-define or .env file.
-  // For Hackathon, you can hardcode it temporarily.
   MapboxOptions.setAccessToken("pk.eyJ1IjoibmlraGlsMjEwMjA2IiwiYSI6ImNta2U0NG0zdTAzMzUzZXMwZjZwbXFzZ3kifQ.fgjpDhGp_9bUapwaLEvtsg");
 
   runApp(const SentraApp());
@@ -22,10 +20,11 @@ class SentraApp extends StatelessWidget {
       title: 'SENTRA',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
+        // Ensure the seed color matches our new Pink theme
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFFFF4081)),
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: const LoginScreen(),
     );
   }
 }
