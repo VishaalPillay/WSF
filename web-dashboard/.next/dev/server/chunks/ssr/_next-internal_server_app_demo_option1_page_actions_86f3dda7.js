c10f7dacf97b19fb1fb0c39a41bd7a32a1bc4e67
Nikhilkarur <karurnikhil2507@gmail.com>
2026-01-20 10:32:52 +0530

    Revert "Built web dashboard and added Supabase schema migration,authentication"

--- web-dashboard/.next/dev/server/chunks/ssr/_next-internal_server_app_demo_option1_page_actions_86f3dda7.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/demo/option1/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_demo_option1_page_actions_86f3dda7.js.map
